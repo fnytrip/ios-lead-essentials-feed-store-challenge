6371379feb145e6da4900e0f02e6828827fe5b25
Fei Wen <fnytrip@gmail.com>
2021-06-05 13:45:38 -0700

    Check `context.hasChanges` before `context.save`  in the `deleteCacheFeed` func since the there could no cache instance thus no need to hit the parent store

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -65,7 +65,9 @@ public final class CoreDataFeedStore: FeedStore {
 		context.perform {
 			do {
 				try ManagedCache.deleteAll(in: context)
-				try context.save()
+				if context.hasChanges {
+					try context.save()
+				}
 				completion(nil)
 			} catch {
 				context.rollback()

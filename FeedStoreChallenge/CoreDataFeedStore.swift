82fd2cf364ab098101a3332ee8723332cd58a25e
Fei Wen <fnytrip@gmail.com>
2021-06-05 02:59:56 -0700

    Implement `CoreDataFeedStore.retrieve()` func

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -30,6 +30,20 @@ public final class CoreDataFeedStore: FeedStore {
 
 	public func retrieve(completion: @escaping RetrievalCompletion) {
 		completion(.empty)
+		let context = self.context
+		context.perform {
+			let request = ManagedCache.fetchRequest() as NSFetchRequest<ManagedCache>
+			do {
+				if let cache = try context.fetch(request).first {
+					let feed = cache.feed.compactMap { $0 as? ManagedFeedImage }.map { LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url) }
+					completion(.found(feed: feed, timestamp: cache.timestamp))
+				} else {
+					completion(.empty)
+				}
+			} catch {
+				completion(.failure(error))
+			}
+		}
 	}
 
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
